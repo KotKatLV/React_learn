922fb9f0169978b5696a668f7e26f04f3222b74b
Константин Константин <kotkat.lv@gmail.com>
2022-02-19 13:49:10 +0300

    Update README.md

--- stardb/src/components/person-details/person-details.js
+++ stardb/src/components/person-details/person-details.js
@@ -1,33 +1,93 @@
 import React, { Component } from 'react';
 
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner";
+
 import './person-details.css';
 
 export default class PersonDetails extends Component {
 
-    render() {
-        return (
-            <div className="person-details card">
-                <img className="person-image"
-                     src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+    swapiService = new SwapiService();
 
-                <div className="card-body">
-                    <h4>R2-D2</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Gender</span>
-                            <span>male</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Birth Year</span>
-                            <span>43</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Eye Color</span>
-                            <span>red</span>
-                        </li>
-                    </ul>
-                </div>
+    state = {
+        person: null,
+        loading: true
+    }
+
+    componentDidMount()
+    {
+        this.updatePerson();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot)
+    {
+        if (this.props.personId != prevProps.personId){
+            this.updatePerson();
+        }
+    }
+
+    onPersonDetailsLoaded = (person) => {
+        this.setState({
+            person: person,
+            loading: false
+        })
+    }
+
+    updatePerson(){
+        const { personId } = this.props;
+        if (!personId){
+            return;
+        }
+        this.swapiService
+            .getPerson(personId)
+            .then(this.onPersonDetailsLoaded);
+    }
+
+    render() {
+
+        if (!this.state.person){
+            return <span>Select a person from a list</span>;
+        }
+
+        const { person, loading } = this.state;
+        const hasData = !loading;
+        const spinner = loading ? <Spinner /> : null;
+        const content = hasData ? <PersonDetailsView person={ person } /> : null;
+
+        return(
+            <div className="person-details card">
+                {spinner}
+                {content}
             </div>
         )
     }
+}
+
+const PersonDetailsView = ({ person }) => {
+    const { id, name, gender, birthYear, eyeColor } = person;
+    return(
+        <React.Fragment>
+            <img className="person-image"
+                 src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+                 alt="character"/>
+
+            <div className="card-body">
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Gender</span>
+                        <span>{gender}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Birth Year</span>
+                        <span>{birthYear}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Eye Color</span>
+                        <span>{eyeColor}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
 }
--- stardb/src/components/random-planet/random-planet.js
+++ stardb/src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 import './random-planet.css';
 
@@ -11,12 +12,17 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
 
-    constructor(){
-        super();
+    componentDidMount(){
         this.updatePlanet();
+        this.interval = setInterval(this.updatePlanet, 10000);
+    }
+
+    componentWillUnmount(){
+
     }
 
     onPlanetLoaded = (planet) =>{
@@ -26,19 +32,31 @@ export default class RandomPlanet extends Component {
         });
     };
 
-    updatePlanet(){
-        const id = 12;
-        this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updatePlanet = () => {
+        const id = Math.floor(Math.random() * 25) + 3;
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
 
     render() {
-
-        const { planet, loading } = this.state;
+        const { planet, loading, error } = this.state;
+        const hasData = !(loading || error);
+        const errorMsg = error ? <ErrorIndicator/> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={planet} /> : null;
+        const content = hasData ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMsg}
                 {spinner}
                 {content}
             </div>
@@ -46,10 +64,9 @@ export default class RandomPlanet extends Component {
     };
 };
 
-const PlanetView = ({ planet }) =>{
+const PlanetView = ({ planet }) => {
     const { id, name, population, rotationPeriod, diameter } = planet;
     return(
-
         <React.Fragment>
             <img className="planet-image"
                  src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
