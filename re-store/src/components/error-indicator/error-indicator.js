98c6690036b3ed39bd16bf36bd7bc06f5e763a8e
KotKatLV <kotkat.lv@gmail.com>
2022-03-01 15:50:18 +0300

    .

---
+++ re-store/src/components/error-indicator/error-indicator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './error-indicator.css';
+import icon from './error-icon.png'
+
+const ErrorIndicator = () => {
+    return(
+        <div className="error-indicator">
+            <img scr={icon} alt="error"/>
+        </div>
+    )
+}
---
+++ re-store/src/components/spinner/spinner.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import './spinner.css'
+
+const Spinner = () => {
+    return(
+        <div class="loadingio-spinner-spin-le20u86tvya">
+            <div class="ldio-7aklm51yv42">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+        </div>
+    </div>
+    )
+}
+
+export default Spinner;
