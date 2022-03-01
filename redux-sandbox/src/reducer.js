c317e9af5e45a1e91bdeaa79fbf03ae6c3120d7f
KotKatLV <kotkat.lv@gmail.com>
2022-03-01 12:53:34 +0300

    .

--- redux-sandbox/src/reducer.js
+++ redux-sandbox/src/reducer.js
@@ -1,10 +1,18 @@
-const reducer = (state = 0, action) => {
-    switch (action.type){
-        case 'RND': return state + action.payload;
-        case 'INC': return state + 1;
-        case 'DEC': return state - 1;
-        default: return state;
-    }
-};
+const reducer = (state = 15, action) => {
 
-export default reducer;
+    switch (action.type) {
+      case 'RND':
+        return state + action.payload;
+  
+      case 'INC':
+        return state + 1;
+  
+      case 'DEC':
+        return state - 1;
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
