0346da2985af937220fdb3f7fdc80d0ff70e1c6f
KotKatLV <kotkat.lv@gmail.com>
2022-03-02 18:09:34 +0300

    135

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import BookListItem from '../book-list-item'
 
@@ -19,4 +20,8 @@ class BookList extends Component {
     }
 }
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+    return { books }
+}
+
+export default connect(mapStateToProps)(BookList);
