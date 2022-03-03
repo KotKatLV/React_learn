ccaaad14565d1d9f967a3da86c8e17b0358e0192
KotKatLV <kotkat.lv@gmail.com>
2022-03-03 16:52:35 +0300

    138

--- re-store/src/components/spinner/spinner.js
+++ re-store/src/components/spinner/spinner.js
@@ -4,8 +4,8 @@ import './spinner.css'
 
 const Spinner = () => {
     return(
-        <div class="loadingio-spinner-spin-le20u86tvya">
-            <div class="ldio-7aklm51yv42">
+        <div className="loadingio-spinner-spin-le20u86tvya">
+            <div className="ldio-7aklm51yv42">
                 <div>
                     <div></div>
                 </div>
