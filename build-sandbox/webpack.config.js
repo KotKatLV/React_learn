71d0d3a79ae2bf6dff40c2a6cd619c429d499549
KotKatLV <kotkat.lv@gmail.com>
2022-03-07 15:33:25 +0300

    170

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -3,12 +3,22 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jpg$/, 
+                test: /\.(jpg|jpeg|png|gif|ico)$/, 
                 use: [{ 
                     loader: 'file-loader' ,
                     options: {
                         outputPath: 'images',
                         name: '[name]-[sha1:hash:7].[ext]'
+                    }
+                }]
+            }
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/, 
+                use: [{ 
+                    loader: 'file-loader' ,
+                    options: {
+                        outputPath: 'fonts',
+                        name: '[name].[ext]'
                     }
                 }]
             }
