2298cd66138904e2466c296de0288f40b46e6ba7
KotKatLV <kotkat.lv@gmail.com>
2022-03-07 17:07:01 +0300

    177

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -35,20 +36,24 @@ module.exports = {
 
             {
                 test: /\.(css)$/,
-                use: [ 'style-loader', 'css-loader']
+                use: [ MiniCssExtractPlugin.loader, 'css-loader']
             },
 
             {
                 test: /\.(s[ca]ss)$/,
-                use: ['style-loader','css-loader','sass-loader']
+                use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
             }
         ]
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Hello World',
             buildTime: new Date().toISOString(),
             template: 'public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main-[hash:8].css'
         })
     ]
 };
